a7dd840b5a1f1ab0604de2a09d993a58a8efa508
agentzz9 <sparsh11194@gmail.com>
2016-03-14 00:04:33 +0530

    Added files via upload

---
+++ 42.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+using namespace std;
+int main()
+{
+    int a=0;
+    while(a!=42)
+    {
+
+        cin>>a;
+        if (a==42)
+        {
+            break;
+        }
+        cout<<a<<endl;
+    }
+    return 0;
+}
+
